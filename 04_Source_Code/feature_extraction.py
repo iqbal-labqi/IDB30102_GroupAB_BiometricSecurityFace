b14d1528469a91144f9637ed265b6529c30fff80
Oji <muhdhussainkoe@gmail.com>
2026-09-11 17:53:56 +0800

    Update feature_extraction.py

--- 04_Source_Code/feature_extraction.py
+++ 04_Source_Code/feature_extraction.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def extract_features(processed_image):
     """
     Preliminary feature extraction component.
@@ -12,7 +11,6 @@ def extract_features(processed_image):
     if processed_image is None:
         raise ValueError("Processed image is required.")
 
-    # Preliminary representation of extracted facial features
     features = np.asarray(processed_image).flatten()
 
     return features
